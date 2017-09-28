df60cbb96edd0052ddd2966a82ba7382fe67c304
Jacek Pietras <jaxilus@gmail.com>
2017-09-28 14:30:29 +0200

    Zakończenie konfiguracji routingu aplikacji

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { EmployeeService } from './services/employee.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuradLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuradLogin,
+    AuthGuardAdmin,
+    EmployeeService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
