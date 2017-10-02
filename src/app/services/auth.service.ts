827a4f8dc7aaddbbb0513c023719f279246f03b5
Jacek Pietras <jaxilus@gmail.com>
2017-10-02 21:23:27 +0200

    Ukończenie pracy nad serwisami, dalsza praca nad front-endem

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,6 +14,45 @@ export class AuthService {
 
     currentUser = {_id: '', username: '', role: ''};
 
-    
+    constructor(private userService: UserService,
+                private router: Router) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decodedUser = this.decodeUserFromToken(token);
+            this.setCurrentUser(decodedUser);
+        }
+    }
+
+    login(emailAndPassword) {
+        return this.userService.login(emailAndPassword).map(res => res.json()).map(
+            res => {
+                localStorage.setItem('token', res.token);
+                const decodedUser = this.decodedUserFromToken(res.token);
+                this.setCurrentUser(decodedUser);
+                return this.loggedIn;
+            }
+        );
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+        this.loggedIn = false;
+        this.isAdmin = false;
+        this.currentUser = { _id: '', username: '', role: '' };
+        this.router.navigate(['/']);
+    }
+
+    decodeUserFromToken(token) {
+        return this.jwtHelper.decodeToken(token).user;
+    }
+
+    setCurrentUser(decodedUser) {
+        this.loggedIn = true;
+        this.currentUser._id = decodedUser._id;
+        this.currentUser.username = decodedUser.username;
+        this.currentUser.role = decodedUser.role;
+        decodedUser.role === 'admin' ? this.isAdmin = true : this.isAdmin = false;
+        delete decodedUser.role;
+    }
 
 }
